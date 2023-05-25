db85d6d99e896d2c57002550b6021074a0ee97c3
MohAlkurdi <mohxxkurdi@gmail.com>
2023-05-25 22:43:47 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const ad3eh = require("./athkar.json");
+
+const app = express();
+
+app.use((req, res, next) => {
+  res.append("Access-Control-Allow-Origin", ["*"]);
+  res.append("Access-Control-Allow-Methods", "GET,PUT,POST,DELETE");
+  res.append("Access-Control-Allow-Headers", "Content-Type");
+  next();
+});
+
+app.get("/morning", (req, res) => {
+  const morningDoaa = ad3eh.morning;
+  res.json(morningDoaa);
+});
+
+app.get("/night", (req, res) => {
+  const nightDoaa = ad3eh.night;
+  res.json(nightDoaa);
+});
+
+app.listen(5000, () => console.log("API server running"));
